89f59d729a51cb4ccf5ba0d1910444a8b091ef01
Theodore Nowak <theodore.nowak@pnnl.gov>
2022-08-23 20:00:14 -0700

    testing web viewer for videos; testing opencv viewer for videos

--- experiments/common/demo_mean_filter.py
+++ experiments/common/demo_mean_filter.py
@@ -3,42 +3,30 @@ import numpy as np
 import cv2
 
 from turbx import REPO_PATH, log
-from turbx.data import prep_exp_data, cap_to_nparray
-from turbx.filter.common import MeanFilter
+from turbx.data import DataLoader, Dataset
+from turbx.filter import common, dft
 from turbx.utils import standard_parser
-from turbx.vis import write_video
+from turbx.vis import view
 
-args = standard_parser()
+# args = standard_parser()
 
 if __name__ == "__main__":
 
-    data_dir = args.data_dir
-    file_name = args.file_name
+    file_path = f"{REPO_PATH}/data/mp4"
+    labels = f"{REPO_PATH}/data/labels"
 
-    vid_path, image_path = prep_exp_data(
-        data_dir, file_name, "/experiments/common/outputs/mean_filter"
-    )
+    dataloader = DataLoader(Dataset(videos=file_path, labels=labels))
 
     # TODO can I get this from video file?
     fps = 10
+    frame_delay = 1.0 / fps
 
-    # create cv video
-    log.info("Opening video...")
-    cap = cv2.VideoCapture(str(vid_path))
+    filter = common.MeanFilter(fps=fps)
 
-    # convert to HSV
-    video = cap_to_nparray(cap, format="HSV")
-    n, h, w, c = video.shape
-    s_channel = video[..., 2].squeeze()
-    s_channel = np.expand_dims(s_channel, axis=-1)
+    print("Calculating filter...")
+    video, label = next(dataloader)
+    filter.calculate(video, fps)
+    out = filter.filter(video)
 
-    # generate background subtraction
-    log.info("Generating Rolling Average filter...")
-    m_filter = MeanFilter(s_channel, fps)
-    background_subtracted = m_filter.apply()
-
-    log.info("Writing to file...")
-    videos = [s_channel, background_subtracted]
-    names = ["demo_raw_video.gif", "demo_background-subtracted_video.gif"]
-    for video, name in zip(videos, names):
-        write_video(video, name, image_path, fps, n)
+    print("Displaying output...")
+    view({"original": video, "mean_filtered": out}, fps)
