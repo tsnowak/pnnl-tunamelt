1767ad60c7045258f1ed3e00edab1bc2147182ef
Theodore Nowak <theodore.nowak@pnnl.gov>
2022-10-26 12:38:33 -0700

    refactored demo files

--- experiments/common/demo_mean_filter.py
+++ experiments/common/demo_mean_filter.py
@@ -1,9 +1,10 @@
+import json
 from pathlib import Path
 from typing import OrderedDict
 from turbx import REPO_PATH, log
 from turbx.data import DataLoader, Dataset, numpy_to_cv2
 from turbx.filter import common
-from turbx.vis import view
+from turbx.vis import viz_video_results
 
 if __name__ == "__main__":
 
@@ -15,53 +16,66 @@ if __name__ == "__main__":
     fps = 10
     frame_delay = 1.0 / fps
 
+    params_path = f"{REPO_PATH}/experiments/best_params.json"
+    with open(params_path, "r") as f:
+        params = json.load(f)
+
     # initialize filters
-    mean_filter = common.MeanFilter(fps=fps)
+    mean_filter = common.MeanFilter(fps=fps, params=params["mean_filter"])
+    contour_filter = common.ContourFilter(params=params["contour_filter"])
 
     # define filter order
-    filter_order = [
-        "original",
-        mean_filter,
-    ]
+    filters = OrderedDict(
+        [
+            ("original", None),
+            ("mean_filter", mean_filter),
+            ("contour_filter", contour_filter),
+        ]
+    )
 
     # get video, label
     video, label = dataloader[3]
     log.info(f"Using video {label['video_id']}...")
 
-    # calculate filters in order
+    # run data through the filters in order
     log.info("Calculating filters...")
     outputs = OrderedDict()
-    for idx, filter in enumerate(filter_order):
-        filter_name = filter if isinstance(filter, str) else filter.__class__.__name__
+    for idx, (filter_name, filter) in enumerate(filters.items()):
         log.info(f"\tCalculating {filter_name}...")
-        if filter == "original":
+        if filter_name == "original":
             outputs["original"] = video[..., 2]
         else:
             tmp = list(outputs.items())[-1]
-            outputs[filter.__class__.__name__] = filter.filter(tmp[1])
+            outputs[filter_name] = filter.filter(tmp[1])
 
     # get filter outputs in order
     display = OrderedDict()
     pred = None
     idx = 0
+    filters_list = list(filters.items())
     for name, output in outputs.items():
         if name == "original":
             display[name] = numpy_to_cv2(video, "HSV", "BGR")
-        elif name == "ContourFilter":
+        elif filters[name].__class__.__name__ == "ContourFilter":
             pred = output
+            # continue
+        elif filters[name].__class__.__name__ == "TrackletAssociation":
+            # pred = output
+            continue
         else:
-            display[name] = numpy_to_cv2(output, filter_order[idx].out_format, "BGR")
+            display[name] = numpy_to_cv2(output, filters_list[idx][1].out_format, "BGR")
         idx += 1
 
-    # display or save filters
-    log.info("Visualizing filter output...")
-    view(
+    # view and save videos with results + save results
+    log.info("Visualizing filter outputs...")
+    viz_video_results(
         display,
         label,
         pred,
         fps,
+        params=params,
         show=True,
         save=False,
         out_path=Path(),
-        video_type=".gif",
+        video_type=".mp4",
     )
--- experiments/dft/multirun.py
+++ experiments/dft/multirun.py
@@ -54,7 +54,7 @@ def generate_param_batches(param_list: Dict) -> List:
 
 
 # load and generate params List(Dict)
-params_path = f"{REPO_PATH}/experiments/dft/params.json"
+params_path = f"{REPO_PATH}/experiments/params.json"
 with open(params_path, "r") as f:
     params_list = json.load(f)
 param_batches = generate_param_batches(params_list)
