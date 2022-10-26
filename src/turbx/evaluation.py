06fc0eff887d97c8e0b21ebeec730ebb708857c0
Theodore Nowak <theodore.nowak@pnnl.gov>
2022-10-26 12:42:52 -0700

    forgot a depricated file

--- src/turbx/evaluation.py
+++
@@ -1,7 +0,0 @@
-"""
-    - Detect (+ track to detect) objects of interest
-    - Remove frames without objects of interest
-    - Load annotations
-    - Determine true/false positives/negatives 
-    - Report
-"""
